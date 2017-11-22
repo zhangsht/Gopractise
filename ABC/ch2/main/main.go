f02d987b37106111c22c075b5c90763b0c23f73e
zhangsht <zhsht201501102104@gmail.com>
2017-11-22 14:23:25 +0800

    uncomment ch2

--- ABC/ch2/main/main.go
+++ ABC/ch2/main/main.go
@@ -1,29 +1,72 @@
 package main
 
 import (
+	"log"
+	"os"
 	"fmt"
-	// "strings"
+	"strings"
 	"flag"
-	// "fmt"
 	"../tempconv"
 )
 
 var n = flag.Bool("n", false, "")
 var sep = flag.String("s", " ", "")
 
+var pc [256]byte
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	count := 0
+	var i uint
+	for ; i < 8; i++ {
+		count += int(pc[byte(x>>(i*8))])
+	}
+	return count
+}
+
+var cwd string
+
+func init() {
+	var err error
+	cwd, err = os.Getwd()
+	if err != nil {
+		log.Fatalf("os Getwd failed: %v", err)
+	}
+}
+
 func main()  {
-	//// pointer
-	// var x, y int
-	// fmt.Println(&x == &x, &x == &y, &x == nil)
+	// pointer
+	var x, y int
+	fmt.Println(&x == &x, &x == &y, &x == nil)
 
-	// // flag
-	// flag.Parse()
-	// fmt.Print(strings.Join(flag.Args(), *sep))
-	// if !*n {
-	// 	fmt.Println()
-	// }
+	// flag
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
 
-	// new and var won't decide the position in memeory
+	//new and var won't decide the position in memeory
 
+	// import other package
 	fmt.Println(tempconv.AbsoluteZeroC)
+
+	// scanf
+	var a int
+	var f float64
+	fmt.Scanf("%d%f", &a, &f)
+	fmt.Println(a, f)
+
+	// init
+	for i, v := range pc {
+		fmt.Printf("num %d has %d 1 bits\n", i, v)
+	}
+	fmt.Println(PopCount(64))
+
+	// global var
+	fmt.Println(cwd)
 }
